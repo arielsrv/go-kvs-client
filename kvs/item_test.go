90c30e0c7f61e012ca1a851e6835693cf974fc80
ariel.pineiro <ariel.pineiro@iskaypet.com>
2024-06-26 09:51:24 +0200

    covearge

---
+++ kvs/item_test.go
@@ -0,0 +1,20 @@
+package kvs_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"gitlab.com/iskaypetcom/digital/sre/tools/dev/go-kvs-client/kvs"
+)
+
+func TestNewItem(t *testing.T) {
+	item := kvs.NewItem("key", `{"name": "value"}`)
+
+	var out map[string]interface{}
+	err := item.TryGetValueAsObjectType(&out)
+
+	require.NoError(t, err)
+	require.Equal(t, "value", out["name"])
+	require.Equal(t, "key", item.Key)
+	require.Equal(t, `{"name": "value"}`, item.Value.(string))
+}
