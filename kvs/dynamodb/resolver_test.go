a1e5d3c7fa13347a394c60ca13c9a0c3aded74da
ariel.pineiro <ariel.pineiro@iskaypet.com>
2025-02-25 23:18:54 +0100

    coverage

---
+++ kvs/dynamodb/resolver_test.go
@@ -0,0 +1,26 @@
+package dynamodb_test
+
+import (
+	"testing"
+
+	aws "github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	smithyendpoints "github.com/aws/smithy-go/endpoints"
+	"github.com/stretchr/testify/require"
+	"gitlab.com/iskaypetcom/digital/sre/tools/dev/go-kvs-client/kvs/dynamodb"
+)
+
+func TestNewResolver(t *testing.T) {
+	resolver := dynamodb.NewResolver("http://localhost:4566")
+
+	endpoint, err := resolver.ResolveEndpoint(t.Context(), aws.EndpointParameters{})
+	require.NoError(t, err)
+	require.Equal(t, "http://localhost:4566", endpoint.URI.String())
+}
+
+func TestNewResolver_Err(t *testing.T) {
+	resolver := dynamodb.NewResolver("::::")
+
+	endpoint, err := resolver.ResolveEndpoint(t.Context(), aws.EndpointParameters{})
+	require.Error(t, err)
+	require.Equal(t, smithyendpoints.Endpoint{}, endpoint)
+}
